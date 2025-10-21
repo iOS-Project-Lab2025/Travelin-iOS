fa5e8e87c163e73eb1f7959b727e674fc5556b65
Rod.Dev <rodolfo.swift.dev@gmail.com>
2025-10-21 15:40:23 -0300

    ad Empty files for Test PR

---
+++ Traveling/TravelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelingApp.swift
+//  Traveling
+//
+//  Created by Rodolfo Gonzalez on 21-10-25.
+//
+
+import SwiftUI
+
+@main
+struct TravelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
