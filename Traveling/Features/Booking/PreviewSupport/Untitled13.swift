329d91aaf3bf1dc325ad140f4c8e5feeccaa941c
rod.dev <rodolfo.swift.dev@gmail.com>
2025-10-22 15:26:27 -0300

    feat: [IT-8] Add project folders (#2)
    * add Empty files for Test PR
    
    * ADD folders Traveling project

---
+++ Traveling/Features/Booking/PreviewSupport/Untitled13.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Traveling
+//
+//  Created by Rodolfo Gonzalez on 21-10-25.
+//
+
---
+++ TravelingTests/TravelingTests.swift
@@ -0,0 +1,16 @@
+//
+//  TravelingTests.swift
+//  TravelingTests
+//
+//  Created by Rodolfo Gonzalez on 21-10-25.
+//
+
+import Testing
+
+struct TravelingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
