a7fb510fc7b4a33282d63902813127e32c727ef8
Rod.Dev <rodolfo.swift.dev@gmail.com>
2025-12-16 00:03:15 -0300

    feat: [IT-137] Implement EndPointBuilderImpTests, and MockEndPoint

---
+++ TravelingTests/Networking/Core/MockEndPoint.swift
@@ -0,0 +1,8 @@
+//
+//  MockEndPoint.swift
+//  TravelingTests
+//
+//  Created by Rodolfo Gonzalez on 15-12-25.
+//
+
+import Foundation
---
+++ TravelingUITests/Test/EndPointBuilderImpTest.swift
@@ -0,0 +1,8 @@
+//
+//  EndPointBuilderImpTest.swift
+//  TravelingUITests
+//
+//  Created by Rodolfo Gonzalez on 15-12-25.
+//
+
+import Foundation
