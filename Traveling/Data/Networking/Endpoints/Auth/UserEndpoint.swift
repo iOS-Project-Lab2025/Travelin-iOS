eafa1a40ebebbaae765a387e1cd51125be2fb630
wim0 <willdmontblanc@gmail.com>
2025-12-21 17:26:14 -0300

    refactor: add register case to UserEndpoint with corresponding path and body data

--- Traveling/Data/Networking/Endpoints/Auth/UserEndpoint.swift
+++ Traveling/Data/Networking/Endpoints/Auth/UserEndpoint.swift
@@ -11,25 +11,25 @@ enum UserEndpoint: EndPointProtocol {
     case login(email: String, password: String)
     case refresh(token: String)
     case me
+    case register(email: String, password: String, firstName: String?, lastName: String?, phone: String?)
 
     var path: String {
         switch self {
         case .login:
             return "/v1/auth/login"
-
         case .refresh:
             return "/v1/auth/refresh"
-
         case .me:
             return "/v1/auth/me"
+        case .register:
+            return "/v1/auth/register"
         }
     }
 
     var method: HTTPMethod {
         switch self {
-        case .login, .refresh:
+        case .login, .refresh, .register:
             return .post
-
         default:
             return .get
         }
@@ -53,10 +53,10 @@ enum UserEndpoint: EndPointProtocol {
         switch self {
         case .login(let email, let password):
             return LoginRequest(email: email, password: password)
-
         case .refresh(let token):
             return RefreshTokenRequest(refreshToken: token)
-
+        case .register(let email, let password, let firstName, let lastName, let phone):
+            return RegisterRequest(email: email, password: password, firstName: firstName, lastName: lastName, phone: phone)
         case .me:
             return nil
         }
